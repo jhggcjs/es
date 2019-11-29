320bcac5fe0202dfb9edabbab49be3e1fe5660e3
e <1774172698@qq.com>
2019-11-29 20:19:58 +0800

    init

--- src/main/java/home/EsApplication.java
+++ src/main/java/home/EsApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EsApplication {
 
     public static void main(String[] args) {
+        System.out.println("111");
         SpringApplication.run(EsApplication.class, args);
     }
 
